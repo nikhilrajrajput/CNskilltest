14963fd5a990d5794819e83caa462cb5d5333a05
nikhilrajrajput <96713971+nikhilrajrajput@users.noreply.github.com>
2023-07-18 17:02:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let [seconds, minutes, hours]=[0,0,0];
+let displayTime= document.getElementById("displayTime");
+let timer=null;
+
+// ------- Program to stopwatch
+function stopWatch() {
+    seconds++;
+    if(seconds==60){
+        seconds = 0;
+        minutes ++;
+        }if (minutes == 60) {
+            minutes = 0;
+            hours ++;
+        }
+
+        let h=hours <10 ? "0" + hours:hours;
+        let m= minutes < 10 ? "0" +minutes:minutes;
+        let s= seconds<10 ? "0" +  seconds:seconds;
+
+        displayTime.innerHTML=h+" : "+m+" : "+s;
+}
+
+// ------ Program to start the watch 
+
+function watchStart() {
+    if (timer!==null) {
+        clearInterval(timer);
+    }
+
+    timer=setInterval(stopWatch,1000);
+        
+}
+// ---------- Program to stop the watch
+function watchStop() {
+    clearInterval(timer);
+}
+
+//  --------- Program to reset the watch
+
+function watchReset() {
+    clearInterval(timer);
+    [seconds,minutes,hours]=[0,0,0];
+    displayTime.innerHTML="00 : 00 : 00";
+}
